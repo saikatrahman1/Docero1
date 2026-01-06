926a55ca40599cb5a51ea6abeb9b22f39c4163c6
Saikat Rahman <saikat.rahman@asdbd.com>
2026-01-06 16:31:57 +0600

    add new endpoint

--- Docero/Program.cs
+++ Docero/Program.cs
@@ -33,6 +33,20 @@ app.MapGet("/weatherforecast", () =>
 })
 .WithName("GetWeatherForecast");
 
+app.MapGet("/weather", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeather");
+
 app.Run();
 
 internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
